5da180760cc90ff0c2cb5f3803b8199ff5d98c67
BuildTools <unconfigured@null.spigotmc.org>
2021-09-15 22:06:15 -0700

    more nsfw

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -23,6 +23,31 @@ class nsfw(commands.Cog):
         res = r.json()
         embed = discord.Embed(title="boobies :D", color=discord.Colour.blurple())
         embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def feet(self, ctx):
+        r = requests.get("https://nekos.life/api/v2/img/feetg")
+        res = r.json()
+        embed = discord.Embed(title="feet. wtf.", color=discord.Colour.red())
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def trap(self, ctx):
+        r = requests.get("https://nekos.life/api/v2/img/trap")
+        res = r.json()
+        embed = discord.Embed(title="trap lol", color=discord.Colour.blurple())
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def femdom(self, ctx):
+        r = requests.get("https://nekos.life/api/v2/img/femdom")
+        res = r.json()
+        embed = discord.Embed(title="femdom", color=discord.Colour.blurple())
+        embed.set_image(url=res['url'])
+        await ctx.send(embed=embed)
 
 
 def setup(client):
