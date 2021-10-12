8a3025e1288dfe622e13b6caffb6109f463401be
aquazarine <chawker374@gmail.com>
2021-10-11 17:07:55 -0700

    added new server logs v2

--- cogs/etc.py
+++ cogs/etc.py
@@ -21,6 +21,17 @@ class etc(commands.Cog):
                                   color=discord.Colour.blurple())
             await webhook.send(embed=embed, username="Aqua Join Logs", avatar_url="https://cdn.discordapp.com/attachments/875661506887426088/897269923636715541/amongie.jpg")
 
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        async with aiohttp.ClientSession() as session:
+            webhook = Webhook.from_url("https://discord.com/api/webhooks/897256513096257556/pyUaTSnhZFFNYiXQ9UEPZ0tdsAoVaNN2jGIju94rvJB8anJGGrfJpxufwwn9Ou759UQ0",
+                                       adapter=AsyncWebhookAdapter(session))
+
+            embed = discord.Embed(title="Bot was removed from a server",
+                                  description=f"servername: {guild.name} \n serverid: {guild.id} \n this is not a poggie moment",
+                                  color=discord.Colour.blurple())
+            await webhook.send(embed=embed, username="Aqua Leave Log", avatar_url="https://cdn.discordapp.com/attachments/875661506887426088/897269923636715541/amongie.jpg")
+
 
 def setup(client):
     client.add_cog(etc(client))
