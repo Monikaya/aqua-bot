dd619e9cc1fd1df4d7619d220f96fff47045549a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-11 19:21:34 -0700

    reverting yk

--- main.py
+++ main.py
@@ -4,16 +4,9 @@ import os
 from itertools import cycle
 import json
 from pretty_help import PrettyHelp, DefaultMenu
-import mysql.connector
 
-db = mysql.connector.connect(
-    host="ger-by-mysql.rawpower.network",
-    port="3306",
-    user="u7298_hMO2hFNjMI",
-    password="=2BattYgeQqtxxNE=dHY^T9C"
-)
 
-cursor = db.cursor()
+# go away
 
 def get_prefix(client, message):
     try:
